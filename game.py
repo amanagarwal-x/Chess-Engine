38aefd09bf922ae9527978e78fdf485831de2a7c
aman <aman.agarwal150@gmail.com>
2021-01-26 00:21:37 +0530

    Updated main loop

--- game.py
+++ game.py
@@ -13,25 +13,22 @@ while(1):
     print("Computer's Move:")
 
     maxScore = float('-inf')
-    nextFen = ""
     for i in possibleStates(initialBoard.fen()): 
-        # print(eval_centerControl(i), materialcount(i))
-        # eval_centerControl(i)
         score = eval_centerControl(i) + materialcount(i)
-        
         if score >= maxScore:
+            maxScore = score
             nextFen = i
 
-   
-    initialBoard = chess.Board(nextFen)
-    # print()
-    # print(initialBoard, "\n")
 
-    # print("Possible Moves: ", initialBoard.legal_moves)
+    initialBoard = chess.Board(nextFen)
+    print()
+    print(initialBoard, "\n")
+
+    print("Possible Moves: ", initialBoard.legal_moves)
     blackMove = input("Enter your move: ")
-    # initialBoard.push_san(blackMove)
-    # print()
-    # print(initialBoard, "\n")
+    initialBoard.push_san(blackMove)
+    print()
+    print(initialBoard, "\n")
 
     
 
