7577b4c189079ec95a4d7a8b67ad43e4f00138f3
mdshah930 <mdshah930@gmail.com>
2021-01-25 19:40:32 +0530

    added init files

--- testarena.py
+++ testarena.py
@@ -1,7 +1,12 @@
+from Aman.main import *
+
 import chess
 
 newboard = chess.Board()
 
-atte4 = newboard.is_attacked_by(chess.WHITE,chess.E4) # returns boolean -> if square is attacked by that piece
-atte3 = newboard.is_attacked_by(chess.WHITE,chess.E3)
-print(atte3)
+print(possibleStates(newboard.fen()))
+
+
+# atte4 = newboard.is_attacked_by(chess.WHITE,chess.E4) # returns boolean -> if square is attacked by that piece
+# atte3 = newboard.is_attacked_by(chess.WHITE,chess.E3)
+# print(atte3)
