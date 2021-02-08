e044dc0f389ec611909286012fe3520ed7370560
Mihir <mdshah930@gmail.com>
2021-02-09 03:07:41 +0530

    sorted pgn on no of moves

--- Database/try_pgn_rread.py
+++ Database/try_pgn_rread.py
@@ -1,22 +1,34 @@
 import chess
 import chess.pgn
 
-f = open("Database\lichess_elite_dec2019.pgn")
 # f = open("Database\master_games.pgn")
-g = open("demo1.txt", "a")
+f = open("Database/pgn_database.txt", "r")
+g = open("Database/sorted_pgn_database.txt","w")
+# f = open("Database/dem1.txt", "r")
+# content = f.read()
 
+dict1={}
+sorteddict1={}
 
-i=0
 while(True):
-    i+=1
-    game = chess.pgn.read_game(f)
-    if game is None:
+    cur_line=f.readline()
+    dict1[cur_line]=len(cur_line)
+    if(f.readline()==""):
         break
-    headers=dict(game.headers)
-    print(game.board().variation_san(game.mainline_moves()))
-    g.write(str(game.board().variation_san(game.mainline_moves())))
-    g.write("\n")
-
 f.close()
-g.close()
-# print(result)
+
+#Sorting dicitonary on values - no of moves(len of line read)
+
+sorteddict1=sorted(dict1.items(), key = lambda kv:(kv[0], kv[1]))
+sorteddict2 = {k: v for k, v in sorted(dict1.items(), key=lambda item: item[1])}
+for pg in reversed(sorteddict2):
+    print(sorteddict2[pg])
+    # print(pg)
+    g.write(pg)
+
+# print(sorteddict2.values())
+# for tup in sorteddict1:
+#     print(tup[1])
+
+
+# print(type(sorteddict1))
